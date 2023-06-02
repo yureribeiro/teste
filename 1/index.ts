3610e64b4bfac7839dbf5c0e989af6ff13e6c1b9
Mac <mac@MacBook-Pro-de-Mac.local>
2023-06-01 23:38:48 -0300

    first commit

--- 1/index.ts
+++ 1/index.ts
@@ -1,1 +1,11 @@
-console.log(`hello world`)
+const user = {
+    username: 'yure',
+    age: 24,
+    address: {
+        street: 'Rua teste'
+    }
+}
+
+const { username } = user
+
+console.log(`hello world ${username}`)
