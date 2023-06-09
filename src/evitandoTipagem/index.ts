286a470843e3749c6338460ae14d1b1e806a6683
Yure Ribeiro <yureribeirocontato@gmail.com>
2023-06-08 21:20:10 -0300

    evitando tipagem

---
+++ src/evitandoTipagem/index.ts
@@ -0,0 +1,22 @@
+function sendSpaceship( spaceship: {pilot: string, copilot?: string} ) {
+  // restante da função
+  // repare que adicionei um '?' ao 'copilot' para indicar que o copilot não é Obrigatório
+}
+
+sendSpaceship({pilot: 'yure', copilot: 'joao'})
+
+sendSpaceship({pilot: 'Lucas'}) //caso NÃO ouvesse o ponto de interrogação o TS reclamaria pois 'copiloto' seria obrigatório..
+
+
+// -------------- ANY 
+// não é recomendado usar o 'any' mas ele serve como evitar tipagem.
+
+let input: any //O tipo 'any' praticamente desabilita o TS, vai aceitar qualquer tipo funcionando como um JS padrão.
+
+input = 'Teste'
+input = 20
+input = []
+
+let text: string
+
+text = input
